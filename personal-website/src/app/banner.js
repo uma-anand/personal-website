338d122e1fb7f53f39a5b23c5d4e8a83c15283c4
Uma <uma2005anand@gmail.com>
2023-07-17 18:55:00 +0530

    feat: resume linking

--- personal-website/src/app/banner.js
+++ personal-website/src/app/banner.js
@@ -3,7 +3,7 @@ export default function Banner() {
       <div className='banner'>
         <h2 id = "title">HOME</h2>
             <h2>PROJECTS</h2>
-            <h2>RESUME</h2>
+            <h2><a target='#' href="https://drive.google.com/file/d/1f5tUWVutXLmoGGiDHJUTKSSAn3JUrYCx/view?usp=sharing">RESUME</a></h2>
             <h2>CONTACT</h2>
       </div>
     )

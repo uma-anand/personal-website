aa263c137cabaa318ac5530d00037f425ee7ca22
Uma <uma2005anand@gmail.com>
2023-07-23 13:09:53 +0530

    feat: web icon and title

--- personal-website/src/app/layout.js
+++ personal-website/src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>Uma Anand</title>
       <body>
         {children}
       </body>
